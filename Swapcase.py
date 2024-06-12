8b7bd81d4fec91fce703c3e364c5e23df84f32dc
Catharin Nivitha <124374895+Catharin-05@users.noreply.github.com>
2024-06-12 14:05:41 +0530

    Create Swapcase.py
    You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.

---
+++ Swapcase.py
@@ -0,0 +1,9 @@
+def swap_case(s):
+  #python built-in funtion to swap cases
+    return s.swapcase()
+    
+# displaying the output for the correspnding input
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
